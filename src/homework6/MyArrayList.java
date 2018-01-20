649e710dcc1a651711b9ce2ef6d4f8d11abff77e
hhlTer <grzc@ukr.net>
2018-01-20 12:35:51 +0200

    *My  - ArrayList  - LinkedList realize. Must correct extend abstract class MyList

--- src/homework6/MyArrayList.java
+++ src/homework6/MyArrayList.java
@@ -1,31 +1,58 @@
 package homework6;
 
 public class MyArrayList<T> extends MyList<T> {
+    private final int BASE_SIZE_ARRAY = 10;
+    private T[] mainArray;
+    private int arraySize = BASE_SIZE_ARRAY;
 
-//    @Override
-//    public void remove(int index){
-//        if (index < 0) throw new ArrayIndexOutOfBoundsException();
-//
-//        Object[] part2 = new Object[size-index-1];
-//        if (index == 0) System.arraycopy(mainArray, 1, mainArray, 0, size--);
-//        else
-//            if (index == size) mainArray[size--] = null;
-//        else //if (index > 0 & index < size)
-//         {
-//            System.arraycopy(mainArray, index + 1 , part2, 0, size-index-1);
-//            System.arraycopy(part2, 0, mainArray, index, --size-index);
-//        }
-//
-//        if (arraySize > 10 & size < (int)(arraySize*0.74)) expanseMainArray();
-//    }
-//
-//
+    public MyArrayList(){
+        mainArray = (T[])new Object[arraySize];
+    }
 
-//    private void dexpanseMainArray(){
-//        arraySize = (int)(size*1.5);
-//        Object[] temp = new Object[arraySize];
-//        System.arraycopy(mainArray, 0, temp, 0, size);
-//        mainArray = new Object[arraySize];
-//        System.arraycopy(temp, 0, mainArray, 0, size);
-//    }
+    @SafeVarargs
+    public MyArrayList(T...t){
+        this.arraySize = (int)(t.length*1.5);
+        mainArray = (T[])new Object[arraySize];
+
+        int count = 0;
+        for (T temp:
+                t) {
+            mainArray[count++] = temp;
+        }
+    }
+    @Override
+    public void add(T t) {
+        mainArray[size++] = t;
+        if (size == arraySize) expanseMainArray();
+    }
+    @Override
+    public void remove(int index){
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (i == index) continue;
+            mainArray[count++] = mainArray[i];
+        }
+        size--;
+    }
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            mainArray[i] = null;
+        }
+        size = 0;
+        expanseMainArray();
+    }
+    @Override
+    public T get(int index){
+        return mainArray[index];
+    }
+
+    private void expanseMainArray(){
+        arraySize = size < BASE_SIZE_ARRAY ? BASE_SIZE_ARRAY : (int)(size*1.5);
+        Object[] temp = new Object[arraySize];
+        System.arraycopy(mainArray, 0, temp, 0, size);
+        mainArray = (T[]) new Object[arraySize];
+        System.arraycopy(temp, 0, mainArray, 0, size);
+    }
+
 }

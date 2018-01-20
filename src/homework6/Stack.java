68c9805a9ece39455aaaf61d9c7bd1d2a9f2692a
hhlTer <grzc@ukr.net>
2018-01-20 18:42:28 +0200

    *My  - ArrayList  - LinkedList realize.
     + Queue
     + Stack
    
     -----------------------------------------
     task: replace remove() and other; - must be not visible for Queue and Stack

---
+++ src/homework6/Stack.java
@@ -0,0 +1,23 @@
+package homework6;
+
+public class Stack<T>  extends MyLinkedList<T> implements Staak<T>, FILO<T>{
+
+    @Override
+    public T pop() {
+        if (size > 0) {
+            T tt = currentEssence.essence;
+            currentEssence = currentEssence.prevEssence;
+            size--;
+            return tt;
+        }
+        else return null;
+    }
+
+    @Override
+    public T peek() {
+        if (size > 0){
+            return currentEssence.essence;
+        }
+        else return null;
+    }
+}
